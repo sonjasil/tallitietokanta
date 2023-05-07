53fb5142f5ee170d77b5525cc0e038f9e571d1a9
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-05-07 12:31:56 +0300

    Fixed error in splitting lines

--- get_lists.py
+++ get_lists.py
@@ -22,21 +22,25 @@ def get_riders():
     return result.fetchall()
 
 def get_lesson_riders(lesson):
-    sql = text(f"SELECT R.rider_name, H.horse_name FROM riders R LEFT JOIN lesson_riders LR ON R.id = LR.rider_id LEFT JOIN horses H ON LR.horse_id = H.id WHERE LR.lesson_id = '{lesson}'")
+    sql = text("SELECT R.rider_name, H.horse_name FROM riders R LEFT JOIN lesson_riders LR ON R.id = LR.rider_id " \
+               f"LEFT JOIN horses H ON LR.horse_id = H.id WHERE LR.lesson_id = '{lesson}'")
     result = db.session.execute(sql)
     return result.fetchall()
 
 def get_own_lessons(user_id):
-    sql = text(f"SELECT R.rider_name, H.horse_name, L.lesson_date, L.lesson_time, L.price FROM lesson_riders LR LEFT JOIN lessons L ON L.id = LR.lesson_id LEFT JOIN riders R ON LR.rider_id = R.id LEFT JOIN horses H ON LR.horse_id = H.id WHERE R.user_id = {user_id}")
+    sql = text("SELECT L.id, R.rider_name, H.horse_name, L.lesson_date, L.lesson_time, L.price FROM lesson_riders LR LEFT JOIN lessons L ON L.id = LR.lesson_id " \
+               f"LEFT JOIN riders R ON LR.rider_id = R.id LEFT JOIN horses H ON LR.horse_id = H.id WHERE R.user_id = {user_id}")
     result = db.session.execute(sql)
     return result.fetchall()
 
 def get_lesson_count(user_id):
-    sql = text(f"SELECT COUNT(LR.lesson_id) FROM riders R LEFT JOIN lesson_riders LR ON R.id = LR.rider_id LEFT JOIN lessons L ON LR.lesson_id = L.id WHERE R.user_id = {user_id}")
+    sql = text("SELECT COUNT(LR.lesson_id) FROM riders R LEFT JOIN lesson_riders LR ON R.id = LR.rider_id " \
+               f"LEFT JOIN lessons L ON LR.lesson_id = L.id WHERE R.user_id = {user_id}")
     result = db.session.execute(sql)
     return result.fetchone()
 
 def get_horse_count(user_id):
-    sql = text(f"SELECT COUNT(DISTINCT LR.horse_id) FROM riders R LEFT JOIN lesson_riders LR ON R.id = LR.rider_id LEFT JOIN lessons L ON LR.lesson_id = L.id WHERE R.user_id = {user_id}")
+    sql = text("SELECT COUNT(DISTINCT LR.horse_id) FROM riders R LEFT JOIN lesson_riders LR ON R.id = LR.rider_id " \
+               f"LEFT JOIN lessons L ON LR.lesson_id = L.id WHERE R.user_id = {user_id}")
     result = db.session.execute(sql)
     return result.fetchone()
